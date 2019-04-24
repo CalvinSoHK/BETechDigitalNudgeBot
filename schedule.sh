39f92bc20333c209456d599afb2f2a54af056dfa
Ryan Pencak <ryanpencak@dhcp-nyc-132.redrover.cornell.edu>
2019-04-24 11:21:04 -0400

    Schedule Shell

--- schedule.sh
+++ schedule.sh
@@ -4,5 +4,8 @@
 # python schedule_reminders.py general "on 04/15/2019 at 7:00pm" "Read Chetty, R. (2015). Behavioral economics and public policy: A pragmatic perspective. American Economic Review."
 # python schedule_messages.py general "04/15/2019 19:00:30" "http://bit.ly/BEtech19-Readings"
 
-python schedule_reminders.py slackbottest "on 04/17/2019 at 9:30am" "Read Chetty, R. (2015). Behavioral economics and public policy: A pragmatic perspective. American Economic Review."
-python schedule_messages.py slackbottest "04/17/2019 9:30:30" "http://bit.ly/BEtech19-Readings"
+python schedule_reminders.py general "on 04/21/2019 at 9:05pm" "Read Open Science Collaboration. (2015). Estimating the reproducibility of psychological science. Science."
+python schedule_messages.py general "04/21/2019 21:06:00" "https://drive.google.com/file/d/1yb0uMuZ0dcRzahI68z8IBT1jilcRsFY_/view?usp=sharing"
+# python schedule_messages.py slackbottest "04/17/2019 9:30:30" "http://bit.ly/BEtech19-Readings"
+
+python schedule_messages.py general "04/22/2019 9:30:00" "Don't forget to review for the BE Tech quiz!"
